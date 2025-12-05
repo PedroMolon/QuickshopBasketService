5bac8124c28dfe5e61d6bb81ee12ba925417d6da
PedroMolon <pedroh.molonr@gmail.com>
2025-12-05 09:27:51 -0300

    first commit

---
+++ src/main/java/com/pedromolon/QuickshopBasketService/QuickshopBasketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pedromolon.QuickshopBasketService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickshopBasketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickshopBasketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedromolon/QuickshopBasketService/QuickshopBasketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromolon.QuickshopBasketService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickshopBasketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
